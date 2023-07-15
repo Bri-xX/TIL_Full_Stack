a5bf297285b64b937cd8f800ed58271d8ee648d0
Bri-xX <brian.xue@mail.utoronto.ca>
2023-07-15 11:23:17 +0800

    added script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+const btn = document.querySelector(".btn-open");
+const form = document.querySelector(".fact-form");
+
+btn.addEventListener("click", function () {
+  if (form.classList.contains("hidden")) {
+    form.classList.remove("hidden");
+    btn.textContent = "Close";
+  } else {
+    form.classList.add("hidden");
+    btn.textContent = "Share a fact";
+  }
+});
+
+function calcFactAge(year) {
+  const currentYear = new Date().getFullYear();
+  const age = currentYear - year;
+
+  if (age >= 0) return age;
+  else return "Impossible year";
+}
