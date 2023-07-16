d67bb78c157a0395155696b0bd7d67590358ba7e
Bri-xX <brian.xue@mail.utoronto.ca>
2023-07-16 15:26:54 +0800

    The app now fetches data from supabase

--- script.js
+++ script.js
@@ -1,6 +1,97 @@
+const CATEGORIES = [
+  { name: "technology", color: "#3b82f6" },
+  { name: "science", color: "#16a34a" },
+  { name: "finance", color: "#ef4444" },
+  { name: "society", color: "#eab308" },
+  { name: "entertainment", color: "#db2777" },
+  { name: "health", color: "#14b8a6" },
+  { name: "history", color: "#f97316" },
+  { name: "news", color: "#8b5cf6" },
+];
+
+const initialFacts = [
+  {
+    id: 1,
+    text: "React is being developed by Meta (formerly facebook)",
+    source: "https://opensource.fb.com/",
+    category: "technology",
+    votesInteresting: 24,
+    votesMindblowing: 9,
+    votesFalse: 4,
+    createdIn: 2021,
+  },
+  {
+    id: 2,
+    text: "Millennial dads spend 3 times as much time with their kids than their fathers spent with them. In 1982, 43% of fathers had never changed a diaper. Today, that number is down to 3%",
+    source:
+      "https://www.mother.ly/parenting/millennial-dads-spend-more-time-with-their-kids",
+    category: "society",
+    votesInteresting: 11,
+    votesMindblowing: 2,
+    votesFalse: 0,
+    createdIn: 2019,
+  },
+  {
+    id: 3,
+    text: "Lisbon is the capital of Portugal",
+    source: "https://en.wikipedia.org/wiki/Lisbon",
+    category: "society",
+    votesInteresting: 8,
+    votesMindblowing: 3,
+    votesFalse: 1,
+    createdIn: 2015,
+  },
+];
+
+//Selecting DOM elements
 const btn = document.querySelector(".btn-open");
 const form = document.querySelector(".fact-form");
+const factsList = document.querySelector(".facts-list");
 
+//Creating DOM elements: Render facts in list
+factsList.innerHTML = "";
+
+//Load data from Supabase
+loadFacts();
+
+async function loadFacts() {
+  const res = await fetch(
+    "https://mckmuqyqbgrbuopgybhw.supabase.co/rest/v1/facts",
+    {
+      headers: {
+        apikey:
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1ja211cXlxYmdyYnVvcGd5Ymh3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODkzMzA2NDksImV4cCI6MjAwNDkwNjY0OX0.4t50RpTJ23neGYDuldVd8WIUeljktd73ujsDBgZjgMo",
+        authorization:
+          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1ja211cXlxYmdyYnVvcGd5Ymh3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODkzMzA2NDksImV4cCI6MjAwNDkwNjY0OX0.4t50RpTJ23neGYDuldVd8WIUeljktd73ujsDBgZjgMo",
+      },
+    }
+  );
+  const data = await res.json();
+  //console.log(data);
+  createFactsList(data);
+}
+
+function createFactsList(dataArr) {
+  const htmlArr = dataArr.map(
+    (fact) => `<li class="fact">${fact.text} 
+  <p>
+    
+      <a
+        class="source"
+        href="${fact.source}"
+        target="_blank"
+      >(Source)</a>
+      </p>
+      <span class="tag" style="background-color: #3b82f6"
+      >${fact.category}</span
+      >
+  </li>`
+  );
+  const html = htmlArr.join("");
+  factsList.insertAdjacentHTML("afterbegin", html);
+}
+
+//Toogle form visibility
 btn.addEventListener("click", function () {
   if (form.classList.contains("hidden")) {
     form.classList.remove("hidden");
@@ -11,10 +102,100 @@ btn.addEventListener("click", function () {
   }
 });
 
-function calcFactAge(year) {
-  const currentYear = new Date().getFullYear();
-  const age = currentYear - year;
+// function calcFactAge(year) {
+//   const currentYear = new Date().getFullYear();
+//   const age = currentYear - year;
 
-  if (age >= 0) return age;
-  else return "Impossible year";
-}
+//   if (age >= 0) return age;
+//   else
+//     return `Impossible year. Year needs to be less or equal ${new Date().getFullYear()}`;
+// }
+
+// const calcFactAge2 = (year) =>
+//   year <= new Date().getFullYear()
+//     ? new Date().getFullYear() - year
+//     : `Impossible year. Year needs to be less or equal ${new Date().getFullYear()}`;
+
+// const fact = ["Lisbon is the capital of Portugal", 2015, true];
+
+// const [text, createdIn, isCorrect] = fact;
+// //console.log(text);
+
+// const newFact = [...fact, "society"];
+
+// const factObj = {
+//   text: "Lisbon is the capital of Portugal",
+//   category: "society",
+//   createdIn: 2015,
+//   isCorrect: true,
+//   createSummary: function () {
+//     return `The fact "${
+//       this.text
+//     }" is from the category ${this.category.toUpperCase()}`;
+//   },
+// };
+
+// //console.log(factObj.createSummary());
+
+// // [2, 3, 4, 5].forEach(function (el) {
+// //   console.log(el);
+// // });
+
+// // const times10 = [2, 3, 4, 5].map(function (el) {
+// //   return el + 10;
+// // });
+
+// // const times10 = [2, 3, 4, 5].map((el) => el * 10);
+// // console.log(times10);
+
+// const CATEGORIES = [
+//   { name: "technology", color: "#3b82f6" },
+//   { name: "science", color: "#16a34a" },
+//   { name: "finance", color: "#ef4444" },
+//   { name: "society", color: "#eab308" },
+//   { name: "entertainment", color: "#db2777" },
+//   { name: "health", color: "#14b8a6" },
+//   { name: "history", color: "#f97316" },
+//   { name: "news", color: "#8b5cf6" },
+// ];
+
+// const allCategories = CATEGORIES.map((el) => el.name);
+// console.log(allCategories);
+
+// const initialFacts = [
+//   {
+//     id: 1,
+//     text: "React is being developed by Meta (formerly facebook)",
+//     source: "https://opensource.fb.com/",
+//     category: "technology",
+//     votesInteresting: 24,
+//     votesMindblowing: 9,
+//     votesFalse: 4,
+//     createdIn: 2021,
+//   },
+//   {
+//     id: 2,
+//     text: "Millennial dads spend 3 times as much time with their kids than their fathers spent with them. In 1982, 43% of fathers had never changed a diaper. Today, that number is down to 3%",
+//     source:
+//       "https://www.mother.ly/parenting/millennial-dads-spend-more-time-with-their-kids",
+//     category: "society",
+//     votesInteresting: 11,
+//     votesMindblowing: 2,
+//     votesFalse: 0,
+//     createdIn: 2019,
+//   },
+//   {
+//     id: 3,
+//     text: "Lisbon is the capital of Portugal",
+//     source: "https://en.wikipedia.org/wiki/Lisbon",
+//     category: "society",
+//     votesInteresting: 8,
+//     votesMindblowing: 3,
+//     votesFalse: 1,
+//     createdIn: 2015,
+//   },
+// ];
+
+// const factAges = initialFacts.map((el) => calcFactAge(el.createdIn));
+// console.log(factAges);
+// console.log(factAges.join(" - "));
