1dece1dc41dbeaaee4ef6d0384a2cbe880d0ce66
Bri-xX <brian.xue@mail.utoronto.ca>
2023-07-21 11:04:54 +0800

    added react

---
+++ v1/README.md
@@ -0,0 +1,3 @@
+# TIL_Full_Stack
+
+A full-stack 'TIL' website
--- v1/script.js
+++ v1/script.js
@@ -67,7 +67,7 @@ async function loadFacts() {
     }
   );
   const data = await res.json();
-  //console.log(data);
+  // const filteredData = data.filter((fact) => fact.category === "society");
   createFactsList(data);
 }
 
@@ -82,7 +82,9 @@ function createFactsList(dataArr) {
         target="_blank"
       >(Source)</a>
       </p>
-      <span class="tag" style="background-color: #3b82f6"
+      <span class="tag" style="background-color: ${
+        CATEGORIES.find((cat) => cat.name === fact.category).color
+      }"
       >${fact.category}</span
       >
   </li>`
